7fdb2d5fccfc67cafe51c6435b5db54150f3dc8c
Abhishek Kagra <AbhishekKagra@aaddoo.net>
2021-07-07 22:14:29 +0530

    uploading files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//navigation bar hamburger toggle funtion
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.nav-list');
+
+hamburger.addEventListener('click', () => {
+  navList.classList.toggle('show');
+}
+)
